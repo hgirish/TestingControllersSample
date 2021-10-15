63bfc9ab1a588c2893ccca4b6488eb0e17a1f330
Girish <girish@post.com>
2021-10-14 19:05:34 -0700

    IdeasController added

---
+++ TestingControllersSample/ClientModels/NewIdeaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestingControllersSample.ClientModels
+{
+    public class NewIdeaModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Range(1, 1000000)]
+        public int SessionId { get; set; }
+    }
+}
